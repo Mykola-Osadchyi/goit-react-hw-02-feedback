f629c9067f69552dd80e216317222f099d1e7781
Mykola Osadchyi <osadchiy_n@ukr.net>
2020-12-10 22:09:45 +0200

    did mentor's remark

--- src/components/Feedbacks/Statistics.js
+++ src/components/Feedbacks/Statistics.js
@@ -2,14 +2,12 @@ import PropTypes from 'prop-types';
 import s from './Feedback.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
-  <div>
-    <div className={s.statistics}>
-      <span>Good: {good}</span>
-      <span>Neutral: {neutral}</span>
-      <span>Bad: {bad}</span>
-      <span>Total: {total}</span>
-      <span>Positive feedback: {Math.round(positivePercentage)}%</span>
-    </div>
+  <div className={s.statistics}>
+    <span>Good: {good}</span>
+    <span>Neutral: {neutral}</span>
+    <span>Bad: {bad}</span>
+    <span>Total: {total}</span>
+    <span>Positive feedback: {Math.round(positivePercentage)}%</span>
   </div>
 );
 
